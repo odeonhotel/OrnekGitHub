5e0a46cded83b9967f766f8f46c87729461f471b
Toshiba <Toshiba@LAPTOP-07R1AJC1>
2017-02-24 12:56:56 +0300

    Add project files.

---
+++ OrnekGitHub/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace OrnekGitHub.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ OrnekGitHub/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace OrnekGitHub.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
